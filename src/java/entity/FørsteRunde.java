e85d78f4288257dbb991865f33fa21045ccbe2f1
Ahmed <Ahmed@Sadiq-PC>
2014-12-12 20:37:49 +0100

    Entity klasse ændret. Controller2 oppe og kører igen.

--- src/java/entity/FørsteRunde.java
+++ src/java/entity/FørsteRunde.java
@@ -62,13 +62,12 @@ public class FørsteRunde implements Serializable {
         this.studentid = studentid;
     }
 
-    public FørsteRunde(Integer studentid, Valgfag førstePrioriteta, Valgfag andenPrioriteta, Valgfag førstePrioritetb, Valgfag andenPrioritetb, Student student) {
+    public FørsteRunde(Integer studentid, Valgfag førstePrioriteta, Valgfag andenPrioriteta, Valgfag førstePrioritetb, Valgfag andenPrioritetb) {
         this.studentid = studentid;
         this.førstePrioriteta = førstePrioriteta;
         this.andenPrioriteta = andenPrioriteta;
         this.førstePrioritetb = førstePrioritetb;
         this.andenPrioritetb = andenPrioritetb;
-        this.student = student;
     }
     
     public Integer getStudentid() {
